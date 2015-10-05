46c2fbc213be839c10c92a029bf423e1f192f9fe
DedoCibula <dedo.cibula@gmail.com>
2015-10-05 12:56:33 -0400

    added initial implementation of LinkedList Node

---
+++ src/main/java/com/javarockstars/mpp/structures/Node.java
@@ -0,0 +1,62 @@
+package com.javarockstars.mpp.structures;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+/**
+ * Author: dedocibula
+ * Created on: 5.10.2015.
+ */
+class Node<K, V> implements Map.Entry<K, V> {
+    final int hash;
+    final K key;
+    V value;
+    AtomicMarkableReference<Node<K, V>> next;
+
+    Node(int hash, K key, V value) {
+        this.hash = hash;
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    void setNext(Node<K, V> next) {
+        Objects.requireNonNull(next);
+        this.next = new AtomicMarkableReference<>(next, false);
+    }
+
+    @Override
+    public String toString() {
+        return key + "=" + value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Object k, v, u; Map.Entry<?,?> e;
+        return ((o instanceof Map.Entry) &&
+                (k = (e = (Map.Entry<?,?>)o).getKey()) != null &&
+                (v = e.getValue()) != null &&
+                (k == key || k.equals(key)) &&
+                (v == (u = value) || v.equals(u)));
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode() ^ value.hashCode();
+    }
+}
