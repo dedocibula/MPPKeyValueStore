ae7f76bc62a0ebef9520c37c6439358d0231ed94
DedoCibula <dedo.cibula@gmail.com>
2015-10-05 16:26:39 -0400

    added dummy test

---
+++ src/test/java/com/javarockstars/mpp/structures/LockFreeKVListTest.java
@@ -0,0 +1,25 @@
+package com.javarockstars.mpp.structures;
+
+import org.junit.Test;
+
+/**
+ * Author: dedocibula
+ * Created on: 5.10.2015.
+ */
+public class LockFreeKVListTest {
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+}
