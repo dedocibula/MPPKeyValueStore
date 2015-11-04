8ebdaf2e5e4fb2fa0b6a7792bab5c62453c9914e
DedoCibula <dedo.cibula@gmail.com>
2015-11-04 17:21:21 -0500

    added ConcurrentMapAdapter

---
+++ src/main/java/com/javarockstars/mpp/benchmarks/ConcurrentMapAdapter.java
@@ -0,0 +1,43 @@
+package com.javarockstars.mpp.benchmarks;
+
+import com.javarockstars.mpp.api.LockFreeMap;
+
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Author: dedocibula
+ * Created on: 4.11.2015.
+ */
+public class ConcurrentMapAdapter<K, V> implements LockFreeMap<K, V> {
+
+    private final ConcurrentMap<K, V> map;
+
+    public ConcurrentMapAdapter(ConcurrentMap<K, V> map) {
+        this.map = map;
+    }
+
+    @Override
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    @Override
+    public boolean put(K key, V value) {
+        return map.put(key, value) == null;
+    }
+
+    @Override
+    public boolean remove(K key) {
+        return map.remove(key) != null;
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+}
