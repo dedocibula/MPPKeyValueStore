e6d324ab678542f6382b72ccc96966606ea792eb
DedoCibula <dedo.cibula@gmail.com>
2015-11-23 22:42:27 -0500

    added tabular dump to file

--- MPPKeyValueStore/src/test/java/com/javarockstars/mpp/benchmarks/BenchmarkInvoker.java
+++ MPPKeyValueStore/src/test/java/com/javarockstars/mpp/benchmarks/BenchmarkInvoker.java
@@ -8,23 +8,34 @@ import org.perfidix.Benchmark;
 import org.perfidix.ouput.TabularSummaryOutput;
 import org.perfidix.result.BenchmarkResult;
 
+import java.io.*;
+import java.util.Objects;
+
 /**
  * This class is responsible for invoking benchmarking evaluation. This is
  * required for the environment that doesn't support the Perfidix plugin
  * directly.
- * 
- * @author shivam.maharshi
  *
+ * @author shivam.maharshi
  */
 public class BenchmarkInvoker {
-	public static void main(String[] args) {
-		Benchmark benchmark = new Benchmark();
-		benchmark.add(ConcurrentHashMapBenchmark.class);
-		benchmark.add(MichaelLockFreeHashMapBenchmark.class);
-		benchmark.add(ConcurrentSkipListHashMapBenchmark.class);
-		benchmark.add(NonBlockingHashMapBenchmark.class);
-		BenchmarkResult result = benchmark.run();
-		new TabularSummaryOutput().visitBenchmark(result);
-	}
+    public static void main(String[] args) throws IOException {
+        Benchmark benchmark = new Benchmark();
+        benchmark.add(ConcurrentHashMapBenchmark.class);
+        benchmark.add(MichaelLockFreeHashMapBenchmark.class);
+        benchmark.add(ConcurrentSkipListHashMapBenchmark.class);
+        benchmark.add(NonBlockingHashMapBenchmark.class);
+        BenchmarkResult result = benchmark.run();
+        new TabularSummaryOutput(toFile("benchmark.txt")).visitBenchmark(result);
+    }
 
+    private static PrintStream toFile(String fileName) throws IOException {
+        Objects.requireNonNull(fileName);
+        File currentDirectory = new File(System.getProperty("user.dir"));
+        File newFile = new File(currentDirectory, fileName);
+        if (!newFile.exists())
+            return (newFile.isDirectory() || !newFile.canWrite()) ? System.out : new PrintStream(new FileOutputStream(newFile, true));
+        else
+            return newFile.createNewFile() ? new PrintStream(newFile) : System.out;
+    }
 }
