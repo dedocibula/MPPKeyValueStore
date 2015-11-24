4560ec2812e1a823682cffd656e1f1a551cd4fc9
DedoCibula <dedo.cibula@gmail.com>
2015-11-24 12:25:02 -0500

    added artificial delay on success

--- src/test/java/com/javarockstars/mpp/benchmarks/datastructures/LockFreeMapBenchmark.java
+++ src/test/java/com/javarockstars/mpp/benchmarks/datastructures/LockFreeMapBenchmark.java
@@ -3,8 +3,10 @@ package com.javarockstars.mpp.benchmarks.datastructures;
 import com.javarockstars.mpp.benchmarks.BenchmarkConstants;
 import com.javarockstars.mpp.datastructures.api.LockFreeMap;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -20,6 +22,7 @@ public abstract class LockFreeMapBenchmark {
 
     private ExecutorService executorService;
     private Collection<Callable<Void>> actions;
+    private Random random;
 
     public LockFreeMapBenchmark(LockFreeMap<String, String> lockFreeMap) {
         this.lockFreeMap = lockFreeMap;
@@ -28,6 +31,7 @@ public abstract class LockFreeMapBenchmark {
     protected void setupBenchmark() {
         executorService = Executors.newFixedThreadPool(BenchmarkConstants.CONCURRENCY);
         actions = new ArrayList<>();
+        random = new Random();
     }
 
     protected void tearDownBenchmark() throws Exception {
@@ -39,16 +43,22 @@ public abstract class LockFreeMapBenchmark {
         for (int i = 0; i < BenchmarkConstants.MAX_KEY; i++) {
             /*
              * Convert integer to string to generate hash code and thereby
-			 * increasing the chances of collisions. Hence replicating real life
-			 * scenarios.
-			 */
-            lockFreeMap.put(Integer.toString(i), BenchmarkConstants.DUMMY_VALUE);
+             * increasing the chances of collisions. Hence replicating real life
+             * scenarios.
+             */
+            delayOnSuccess(lockFreeMap.put(Integer.toString(i), BenchmarkConstants.DUMMY_VALUE));
+        }
+    }
+
+    protected void getValues() {
+        for (int i = 0; i < BenchmarkConstants.MAX_KEY; i++) {
+            lockFreeMap.get(Integer.toString(i));
         }
     }
 
     protected void clearMap() {
         for (int i = 0; i < BenchmarkConstants.MAX_KEY; i++) {
-            lockFreeMap.remove(Integer.toString(i));
+            delayOnSuccess(lockFreeMap.remove(Integer.toString(i)));
         }
     }
 
@@ -60,16 +70,18 @@ public abstract class LockFreeMapBenchmark {
         }
     }
 
-    protected void getValues() {
-        for (int i = 0; i < BenchmarkConstants.MAX_KEY; i++) {
-            lockFreeMap.get(Integer.toString(i));
-        }
-    }
-
     protected void setupActions(Runnable action) {
         actions.clear();
         IntStream.range(0, BenchmarkConstants.CONCURRENCY)
                 .mapToObj(i -> (Callable) Executors.callable(action))
                 .forEach(actions::add);
+    }
+
+    private void delayOnSuccess(boolean success) {
+        try {
+            if (success)
+                Thread.sleep(Duration.ofNanos(Math.abs(random.nextInt(200))).toMillis());
+        } catch (InterruptedException ignored) {
+        }
     }
 }
